5317e31e1dfdb8a86546dbba155af89c8cc16c2f
Aymane Hachcham <aymanehachcham@Aymanes-M1-pro.local>
2023-01-26 22:07:14 +0100

    Bert added in embeddings

--- Foundations/embeddings.py
+++ Foundations/embeddings.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import torch
+
 from foundations import Tokenize
 import gensim
 from gensim.models.doc2vec import TaggedDocument
+from typing import Literal
+
 
 class VectorEmbeddings():
     def __init__(
@@ -9,12 +13,16 @@ class VectorEmbeddings():
         df:pd.Series,
         doc_vect_size:int,
         min_freq_word_count:int=2,
+        num_docs:int=100,
+        embedding:Literal['doc2vec', 'bert']='doc2vec'
     ):
         self._tokens = []
         self.model = None
         self.df = df
         self.min_freq_word_count = min_freq_word_count
         self.vocab:bool=False
+        self.num_docs = num_docs
+        self.embedding = embedding
 
         if doc_vect_size < 100:
             self.doc_vect_size = 100
@@ -33,8 +41,19 @@ class VectorEmbeddings():
             normalized=True
         ).get_tokens()
 
+
+    def bert_case_preparation(self):
+        from transformers import BertTokenizer, BertModel
+        self.bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.model = BertModel.from_pretrained(
+            'bert-base-uncased',
+            output_hidden_states = True,
+        )
+        self.model.eval()
+
+
     def _preprocess_documents(self):
-        self.tokens = 100
+        self.tokens = self.num_docs
         for i, doc in enumerate(self._tokens):
             yield gensim.models.doc2vec.TaggedDocument(doc, [i])
 
@@ -48,18 +67,64 @@ class VectorEmbeddings():
         self.model.build_vocab(self.train_corpus)
         self.vocab = True
 
-    def infer_vector(self, doc:pd.Series, index:int):
+    def fit_model(self):
         self._build_vocab()
         self.model.train(
             self.train_corpus,
             total_examples=self.model.corpus_count,
             epochs=self.model.epochs
         )
+        self.vocab = True
 
-        doc = Tokenize(
-            series=doc[index-1:index],
-            stopwords=True,
-            normalized=True
-        ).get_tokens()[0]
+    def infer_vector(self, doc:pd.Series, index:int):
 
-        return self.model.infer_vector(doc)
+        if self.embedding == 'doc2vec':
+            if self.vocab:
+                doc_tokenized = Tokenize(
+                    series=doc[index - 1:index],
+                    stopwords=True,
+                    normalized=True
+                ).get_tokens()[0]
+
+                return self.model.infer_vector(doc_tokenized)
+
+        if self.embedding == 'bert':
+            sentence_inputs = list(filter(lambda x:x!='', doc[index - 1:index].tolist()[0].split('.')))
+            doc_input = ''.join(sentence_inputs[:8])
+
+            marked_text = "[CLS] " + doc_input + " [SEP]"
+            tokens = self.bert_tokenizer.tokenize(marked_text)
+            idx = self.bert_tokenizer.convert_tokens_to_ids(tokens)
+            segment_id = [1] * len(tokens)
+
+            self.tokens_tensor = torch.tensor([idx])
+            self.segments_tensors = torch.tensor([segment_id])
+
+            with torch.no_grad():
+                outputs = self.model(self.tokens_tensor, self.segments_tensors)
+                hidden_states = outputs[2]
+
+            self.hidden_states = hidden_states
+
+            return torch.mean(self.hidden_states[-2][0], dim=0)
+
+
+
+df = pd.read_csv('data.csv')['review']
+doc = df[10:11]
+
+token_model = VectorEmbeddings(df, 100, embedding='doc2vec')
+token_model.fit_model()
+print(token_model.infer_vector(df, 100))
+
+
+
+
+
+
+
+
+
+
+
+
